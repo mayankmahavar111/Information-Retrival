b1f7dd1c27e712c780430a1a20b8a99563b82c52
Manu9948 <manoharmadanu@gmail.com>
2018-03-26 23:23:34 +0530

    manohar
    query

--- interface/search/views.py
+++ interface/search/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 from search.forms import querystr
 from django.views.generic import View
 from django.shortcuts import render,redirect
+import sqlite3
 
+query=""
 class index(View):
     form_class=querystr
     template_name='search/search.html'
@@ -12,16 +14,27 @@ class index(View):
         return render(request,self.template_name,{'form':form})
 
     def post(self, request):
+        global query
         form = self.form_class(request.POST)
         print "hello"
         if form.is_valid():
-            #print "Hello World"
             query = form.cleaned_data['query']
-            print query
+            #print query
             return redirect('/search/result')
         else:
             return render(request, self.template_name, {'form': form})
 
-
 def result(request):
+    print query
+    db_name = 'document'
+    conn = sqlite3.connect('{}.db'.format(db_name))
+    db = conn.cursor()
+
+    sql = """select TRAIN_id from TRAIN where body like "%{}%" """.format(query)
+
+
+    res = db.execute(sql)
+
+    temp = res.fetchall()
+    print temp
     return render(request,'search/result.html')
